26397bd5ed9f69c15dad497f4d723dc4acb20b3f
mxchan <mingxiangchan@gmail.com>
2017-05-15 14:01:01 +0800

    basic endpoint for logging in via api

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < Api::V1::ApplicationController
+  def create
+    email, password = session_params.values_at("email", "password")
+    user = User.authenticate!(email, password)
+    if user
+      render json: {private_token: user.private_token}
+    else
+      render status: 403, json: {error: "Wrong email or password"}
+    end
+  end
+
+  private
+  def session_params
+    params.permit(:email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   before_save :generate_private_token
   has_many :claims
 
+  def self.authenticate!(email, password)
+    user = User.find_by(email: email)
+    return false if user.nil? || (user.password != password)
+    return user
+  end
+
   def generate_private_token
     self.private_token = SecureRandom.hex(10) if private_token.blank?
   end
